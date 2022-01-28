5014b441fc3c0b84ffc49fe572cbc1b990cd5a7c
fdn <fisenko@signatec.ru>
2022-01-28 18:04:12 +0700

    fix

--- pub/build.sh
+++ pub/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-dotnet publish -o ./linux-x64 -r linux-x64  -c Release --self-contained true --use-current-runtime -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true ../src/AHashPassGen/AHashPassGen.csproj
-dotnet publish -o ./win-x64 -r win-x64  -c Release --self-contained true --use-current-runtime -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true ../src/AHashPassGen/AHashPassGen.csproj
+dotnet publish -o ./linux-x64 -r linux-x64  -c Release --self-contained true  -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true -p:PublishReadyToRun=true ../src/AHashPassGen/AHashPassGen.csproj
+dotnet publish -o ./win-x64 -r win-x64  -c Release --self-contained true  -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true -p:PublishReadyToRun=true ../src/AHashPassGen/AHashPassGen.csproj
+#dotnet publish -o ./win-x64-dist -r win-x64 --self-contained false -c Release ../src/AHashPassGen/AHashPassGen.csproj
+
+
+#--use-current-runtime
+#-p:PublishTrimmed=true
 
 rm ./linux-x64/*.pdb
 rm ./win-x64/*.pdb
+rm ./win-x64-dist/*.pdb
